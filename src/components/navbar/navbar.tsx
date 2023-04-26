4d97c5e7b6c124d70ce25d120b03006e6a6a4297
AngryKat <gatto_candido@ukr.net>
2023-04-26 23:39:34 +0300

    Rewrite navbar with radix

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,33 +1,53 @@
-import Image from "next/image"
-import Link from "next/link"
-import styles from "./styles.module.scss"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { CaretDownIcon } from "@radix-ui/react-icons";
+import styles from "./styles.module.scss";
+
+interface NavItem {
+  name: string;
+  href: string;
+}
+
+const navigationItems: NavItem[] = [
+  { name: "Docs", href: "docs" },
+  { name: "Templates", href: "templates" },
+  { name: "Integrations", href: "integrations" },
+  { name: "Customers", href: "customers" },
+  { name: "Enterprise", href: "enterprise" },
+  { name: "Pricing", href: "pricing" },
+];
 
 export default function NavBar() {
-    return (
-        <header className={styles['nav-bar']}>
-            <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
-                priority
-            />
-            <nav>
-                <ul className="flex">
-                    <li>Features</li>
-                    <li>Docs</li>
-                    <li>Templates</li>
-                    <li>Integrations</li>
-                    <li>Customers</li>
-                    <li>Enterprise</li>
-                    <li>Pricing</li>
-                </ul>
-            </nav>
-            <div>
-                <Link href="/">Contact</Link>
-                <Link href="/">Login</Link>
-            </div>
-        </header>
-    )
+  return (
+    <header className={styles.NavBar}>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
+      <NavigationMenu.Root>
+        <NavigationMenu.List className="flex">
+          <NavigationMenu.Item>
+            <NavigationMenu.Trigger>
+              Features <CaretDownIcon className={styles.CaretDown} />
+            </NavigationMenu.Trigger>
+          </NavigationMenu.Item>
+          {navigationItems.map(({ name, href }) => (
+            <NavigationMenu.Item>
+              <Link href={href}>{name}</Link>
+            </NavigationMenu.Item>
+          ))}
+        </NavigationMenu.List>
+      </NavigationMenu.Root>
+      <div>
+        <Link href="/">Contact</Link>
+        <Link href="/">Login</Link>
+      </div>
+    </header>
+  );
 }
