81db11d2a744838741321058860ab0d3feb2cfab
AngryKat <gatto_candido@ukr.net>
2023-04-30 11:04:00 +0300

    Refactor

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import { CaretDownIcon } from "@radix-ui/react-icons";
-import styles from "./styles.module.scss";
+import navbar from "./styles.module.scss";
+import classNames from "classnames";
 
 interface NavItem {
   name: string;
@@ -21,26 +22,17 @@ const navigationItems: NavItem[] = [
 
 const SignUpButtonLink = () => {
   return (
-    <Link href="signup"
-      className="flex items-center"
-      style={{
-        backgroundColor: 'white',
-        color: 'black',
-        borderRadius: 5,
-        padding: '0 12px',
-        margin: '0 12px',
-        whiteSpace: 'nowrap',
-        height: '100%',
-      }}>
-      <span>
-        Sign Up
-      </span>
+    <Link
+      href="signup"
+      className={classNames("flex items-center text-center", navbar.SignUpButtonLink)}
+    >
+      <span>Sign Up</span>
     </Link>)
 }
 
 const NavigationLinkItem = ({ name, href }: NavItem) => (
   <NavigationMenu.Item>
-    <Link href={href} style={{ padding: '8px 12px' }}>
+    <Link href={href} className="py-2 px-3">
       {name}
     </Link>
   </NavigationMenu.Item>
@@ -48,51 +40,44 @@ const NavigationLinkItem = ({ name, href }: NavItem) => (
 
 const NavigationTriggerItem = () => (
   <NavigationMenu.Item key="Features--1">
-    <NavigationMenu.Trigger className="flex items-center" style={{ padding: '8px 12px' }}>
-      Features <CaretDownIcon className={styles.CaretDown} />
+    <NavigationMenu.Trigger className="flex items-center px-3 py-2">
+      Features <CaretDownIcon className={navbar.CaretDown} />
     </NavigationMenu.Trigger>
   </NavigationMenu.Item>
 )
 
 export default function NavBar() {
   return (
-      <header className="flex items-center justify-center" style={{ minHeight: 64, width: '100%' }}>
-        <div className="flex items-center  px-6" style={{ fontSize: 14, width: 1248 }}>
-          <div className="flex mr-auto" style={{ flex: 1 }}>
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </div>
-          <div className="flex justify-center">
-            <NavigationMenu.Root>
-              <NavigationMenu.List className="flex items-center justify-center">
-                <NavigationTriggerItem />
-                {navigationItems.map((item) => (
-                  <NavigationLinkItem {...item} key={item.name} />
-                ))}
-              </NavigationMenu.List>
-            </NavigationMenu.Root>
-          </div>
-          <div className="flex items-center ml-auto justify-end" style={{ flex: 1, height: 32 }}>
-            <div>
-              <Link href="/"
-                style={{
-
-                  padding: '0 12px'
-                }}>Contact</Link>
-              <Link href="/"
-                style={{
-                  padding: '0 12px'
-                }}>Login</Link>
-            </div>
-            <SignUpButtonLink />
-          </div>
+    <header className={classNames("flex items-center justify-center w-full", navbar.NavHeader)}>
+      <div className="flex items-center text-sm w-10/12">
+        <div className="flex mr-auto" style={{ flex: 1 }}>
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className="dark:invert"
+            width={115}
+            height={26}
+            priority
+          />
         </div>
-      </header>
+        <div className="flex justify-center">
+          <NavigationMenu.Root>
+            <NavigationMenu.List className="flex items-center justify-center  gap-x-2">
+              <NavigationTriggerItem />
+              {navigationItems.map((item) => (
+                <NavigationLinkItem {...item} key={item.name} />
+              ))}
+            </NavigationMenu.List>
+          </NavigationMenu.Root>
+        </div>
+        <div className="flex items-center ml-auto justify-end" style={{ flex: 1, height: 32 }}>
+          <div>
+            <Link href="/" className="px-3">Contact</Link>
+            <Link href="/" className="px-3">Login</Link>
+          </div>
+          <SignUpButtonLink />
+        </div>
+      </div>
+    </header>
   );
 }
